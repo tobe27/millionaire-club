b1c50d47acc530a9ec43ae48d283149bf6b80b5e
qiaobaojoe <1139987421@qq.com>
2018-08-17 04:10:19 +0800

    dao完成-----major joe

--- millionaire-admin-service/src/main/java/com/millionaire/millionaireserverweb/MillionaireServerWebApplication.java
+++ millionaire-admin-service/src/main/java/com/millionaire/millionaireserverweb/MillionaireServerWebApplication.java
@@ -1,5 +1,6 @@
 package com.millionaire.millionaireserverweb;
 
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- millionaire-business-service/src/main/java/com/millionaire/millionairebusinessservice/MillionaireBusinessServiceApplication.java
+++ millionaire-business-service/src/main/java/com/millionaire/millionairebusinessservice/MillionaireBusinessServiceApplication.java
@@ -1,9 +1,11 @@
 package com.millionaire.millionairebusinessservice;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.millionaire.millionairebusinessservice.dao")
 public class MillionaireBusinessServiceApplication {
 
 	public static void main(String[] args) {
