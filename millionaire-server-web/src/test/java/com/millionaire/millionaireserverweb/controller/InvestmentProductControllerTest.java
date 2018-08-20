e099db09bab3a4e65b07c5f1eafc643342194ce7
Liuk2018 <liuk2018@aliyun.com>
2018-08-20 22:32:38 +0800

    20180820 投资产品模块逻辑基本实现，用户查询模块需要完善数据

--- millionaire-server-web/src/test/java/com/millionaire/millionaireserverweb/controller/InvestmentProductControllerTest.java
+++ millionaire-server-web/src/test/java/com/millionaire/millionaireserverweb/controller/InvestmentProductControllerTest.java
@@ -2,13 +2,17 @@ package com.millionaire.millionaireserverweb.controller;
 
 
 import com.github.pagehelper.PageInfo;
+import com.millionaire.millionairebusinessservice.dao.InvestmentProductMapper;
 import com.millionaire.millionairebusinessservice.module.InvestmentProduct;
 import com.millionaire.millionairebusinessservice.service.InvestmentProductService;
+import org.assertj.core.internal.Integers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -17,9 +21,20 @@ public class InvestmentProductControllerTest {
    @Autowired
    InvestmentProductService service;
 
+   @Autowired
+    InvestmentProductMapper mapper;
+
     @Test
     public void getListInvestmentProduct() {
         PageInfo<InvestmentProduct> pageInfo=service.selectByPage(1,1);
         System.err.println(pageInfo);
     }
+
+    @Test
+    public void mapperTest() {
+        List<InvestmentProduct> list=mapper.selectAll();
+        System.out.println(list);
+    }
+
+
 }
---
+++ millionaire-user-service/src/main/java/com/millionaire/millionaireuserservice/service/impl/ReceptionUsersServiceImpl.java
@@ -0,0 +1,60 @@
+package com.millionaire.millionaireuserservice.service.impl;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.millionaire.millionaireuserservice.dao.ReceptionUsersMapper;
+import com.millionaire.millionaireuserservice.module.ReceptionUsers;
+import com.millionaire.millionaireuserservice.module.ReceptionUsersQuery;
+import com.millionaire.millionaireuserservice.service.ReceptionUsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author Liu Kai
+ * @Description: TODO 投资用户 查询 修改
+ * @date 2018/8/20 12:40
+ */
+
+@Service
+public class ReceptionUsersServiceImpl implements ReceptionUsersService {
+    @Autowired
+    ReceptionUsersMapper usersMapper;
+
+    /**
+     * @param usersQuery 用户查询所需参数
+     * @param pageSize   每页数据
+     * @param pageNum    页码
+     * @return 分页数据
+     * @Description 分页查询投资用户信息
+     **/
+    @Override
+    public PageInfo<ReceptionUsers> selectProductByPage(ReceptionUsersQuery usersQuery, Integer pageSize, Integer pageNum) {
+        PageHelper.startPage(pageNum, pageSize);
+        List<ReceptionUsers> usersList = usersMapper.selectUserByPage(usersQuery);
+        PageInfo<ReceptionUsers> pageInfo = new PageInfo<>(usersList);
+        return pageInfo;
+    }
+
+    /**
+     * @param id 用户id
+     * @return 投资用户
+     * @Description 根据id查询用户
+     **/
+    @Override
+    public ReceptionUsers selectByPrimaryKey(Long id) {
+        return usersMapper.selectByPrimaryKey(id);
+    }
+
+    /**
+     * @param record 投资用户
+     * @return 受影响行数
+     * @Description 编辑投资用户 不限定参数 封装更新时间
+     **/
+    @Override
+    public int updateByPrimaryKeySelective(ReceptionUsers record) {
+        record.setGmtUpdate(System.currentTimeMillis());
+        return usersMapper.updateByPrimaryKeySelective(record);
+    }
+}
