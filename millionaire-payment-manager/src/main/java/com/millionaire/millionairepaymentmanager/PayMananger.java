ccba151f4ea5205390181faa0568eaf221ff3e8c
qiaobaojoe <1139987421@qq.com>
2018-08-21 15:31:34 +0800

    更新提交  ----majorJoe

--- millionaire-payment-manager/src/main/java/com/millionaire/millionairepaymentmanager/PayMananger.java
+++ millionaire-payment-manager/src/main/java/com/millionaire/millionairepaymentmanager/PayMananger.java
@@ -1,7 +1,18 @@
 package com.millionaire.millionairepaymentmanager;
+import java.util.Date;
+
+import com.millionaire.millionairebusinessservice.module.InvestmentProduct;
+import com.millionaire.millionairebusinessservice.module.InvestmentUser;
+import com.millionaire.millionairebusinessservice.service.InvestmentProductService;
+import com.millionaire.millionairepaymentmanager.requst.UserInvestmentRequestBean;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class PayMananger {
 
+    @Autowired
+    private InvestmentProductService investmentProductService;
+
+    InvestmentUser investmentUser = new InvestmentUser();
 
     /**
      *交易流程管理：
@@ -11,9 +22,31 @@ public class PayMananger {
      * if1.支付成功调用定时任务接口、用户消息推送、交易流水表数据插入
      * else 2.支付失败用户消息推送、交易流水表数据插入
      */
-    public void payManager() {
+    public void payManager(UserInvestmentRequestBean requestBean,long uid) {
+//        查询购买的产品信息
+        InvestmentProduct investmentProduct=investmentProductService.selectByPrimaryKey(requestBean.getProductId());
+
+        investmentUser.setProductId(requestBean.getProductId());
+        investmentUser.setUid(uid);
+        investmentUser.setValueDateStart(new Date());
+        investmentUser.setValueDateEnd(new Date());
+        investmentUser.setLendingContractNumber("");
+        investmentUser.setContractSign("");
+        investmentUser.setInvestmentAmount(0);
+        investmentUser.setInvestmentStatus((byte)0);
+        investmentUser.setProjectStatus((byte)0);
+        investmentUser.setUndistributedIncome(0.0D);
+        investmentUser.setDistributedIncome(0.0D);
+        investmentUser.setTranslationDate(new Date());
+        investmentUser.setGmtCreate(0L);
+        investmentUser.setGmtUpdate(0L);
+        investmentUser.setClaimId("");
+
+
 
     }
+
+
 
 
 }
