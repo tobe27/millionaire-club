3c483bac194756dc27592f369634a4ba564ee75f
qiaobaojoe <1139987421@qq.com>
2018-08-21 09:02:35 +0800

    ...  ----majorJoe

--- millionaire-client-web/src/main/java/com/millionaire/millionaireclientweb/exception/GlobalExceptionHandler.java
+++ millionaire-client-web/src/main/java/com/millionaire/millionaireclientweb/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.millionaire.millionaireclientweb.exception;
 
 import com.millionaire.millionaireclientweb.result.ErrorInfo;
 import com.millionaire.millionairecommonapi.excepion.AliyunAPIException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,10 +16,14 @@ import javax.servlet.http.HttpServletRequest;
  * @InitBinder和@ModelAttribute方法，适用于所有使用@RequestMapping方法。
  */
 public class GlobalExceptionHandler {
+
+    private Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(value = AliyunAPIException.class)
     @ResponseBody
     public ErrorInfo<String> jsonErrorHandler(HttpServletRequest req, AliyunAPIException e) throws Exception {
         ErrorInfo<String> r = new ErrorInfo<>();
+        logger.info("============aliyun图片上传错误=========");
         r.setMessage(e.getMessage());
         r.setCode(ErrorInfo.ERROR);
         r.setData("new data");
---
+++ millionaire-payment-manager/src/main/java/com/millionaire/millionairepaymentmanager/PayMananger.java
@@ -0,0 +1,21 @@
+package com.millionaire.millionairepaymentmanager;
+
+public class PayMananger {
+
+
+    /**
+     *交易流程管理：
+     * 1.根据productId 查询产品信息
+     * 2.计算：用户投资收益、起息和到期时间、用户合同编号生成、用户投资表默认状态写入
+     * 3.调用富友支付接口，
+     * if1.支付成功调用定时任务接口、用户消息推送、交易流水表数据插入
+     * else 2.支付失败用户消息推送、交易流水表数据插入
+     */
+    public void payManager() {
+
+
+
+    }
+
+
+}
