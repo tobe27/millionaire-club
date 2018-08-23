abbbe73224a036199d3bef157622c542f2c13c53
qiaobaojoe <1139987421@qq.com>
2018-08-24 04:18:37 +0800

    上传

--- millionaire-payment-manager/src/main/java/com/millionaire/millionairepaymentmanager/PayMananger.java
+++ millionaire-payment-manager/src/main/java/com/millionaire/millionairepaymentmanager/PayMananger.java
@@ -20,6 +20,8 @@ public class PayMananger {
     private void payManager(UserInvestmentRequestBean requestBean, long uid) {
 //        生成用户投资信息
         investmentUserInsert.investmentUserInsert(requestBean, uid);
+//        生成交易记录
+//        用户消息推送
 //        调用支付接口
 
     }
