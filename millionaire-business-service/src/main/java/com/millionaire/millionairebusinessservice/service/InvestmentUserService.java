75741d43471bdf30c4b93bae3c0abcb8b7ebfab9
Liuk2018 <40117019+Liuk2018@users.noreply.github.com>
2018-08-24 17:31:28 +0800

    Update InvestmentUserService.java

--- millionaire-business-service/src/main/java/com/millionaire/millionairebusinessservice/service/InvestmentUserService.java
+++ millionaire-business-service/src/main/java/com/millionaire/millionairebusinessservice/service/InvestmentUserService.java
@@ -1,23 +1,5 @@
-package com.millionaire.millionairebusinessservice.service;
-
-import com.millionaire.millionairebusinessservice.module.InvestmentUser;
-import com.millionaire.millionairebusinessservice.request.InvestmentUserQuery;
-import com.millionaire.millionairebusinessservice.transport.InvestmentUserDTO;
-
-import java.util.List;
-
-/**
- * @author Liu Kai
- * @Description: TODO 用户交易信息查询
- * @date 2018/8/24 11:09
- */
-public interface InvestmentUserService {
-    /**
-     * @Description 通过查询参数查询用户投资记录
-     * @author Liu Kai
-     **/
-    List<InvestmentUserDTO> listInvestmentUserByQuery(InvestmentUserQuery query);
 
 
 
-}
+
+
