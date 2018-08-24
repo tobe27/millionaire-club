560b74e011e3fcc57db11c52bc757774914b7013
qiaobaojoe <1139987421@qq.com>
2018-08-24 16:06:16 +0800

    clean

--- millionaire-admin-service/src/main/java/com/millionaire/millionaireadminservice/config/PutFilter.java
+++
@@ -1,7 +0,0 @@
-package com.millionaire.millionaireadminservice.config;
-
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.HttpPutFormContentFilter;
-    @Component
-    public class PutFilter extends HttpPutFormContentFilter {
-}
