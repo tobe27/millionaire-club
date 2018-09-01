476d48dcb59ba765599ed4bb4649180fc71dfb2f
qiaobaojoe <1139987421@qq.com>
2018-09-01 20:46:35 +0800

    提交

--- millionaire-quartz-manager/src/main/java/com/millionaire/millionairequartzmanager/job/InvestmentUserJob.java
+++ millionaire-quartz-manager/src/main/java/com/millionaire/millionairequartzmanager/job/InvestmentUserJob.java
@@ -14,6 +14,7 @@ import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -52,10 +53,48 @@ public class InvestmentUserJob implements Job {
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
 //        扫描获得所有当前需要执行的定时任务
         List<TimerTaskInvestment> listTaskForExecute = taskInvestmentService.listTimerTaskForExecute();
-//        10代表本息一次回款
-//        20代表分期还息
-//        30代表最后一次还息回款
-//        40表示用户投资到期后
+
+        //Output : C
+        listTaskForExecute.forEach(taskInvestment->{
+            if(taskInvestment.getExecuteType() == 10){//        10代表本息一次回款
+//                生成交易流水记录
+//                调用支付接口转账
+//                修改定时任务状态
+//                更新用户账户的余额和收益
+//                更新用户投资状态
+//                取消债权匹配
+//                生成用户消息
+            }
+            if(taskInvestment.getExecuteType() == 20){//        20代表分期还息
+                System.out.println(taskInvestment);
+            }
+            if(taskInvestment.getExecuteType() == 30){//        30代表最后一次还息回款
+                System.out.println(taskInvestment);
+            }
+            if(taskInvestment.getExecuteType() == 40){//        40表示用户投资到期后
+                System.out.println(taskInvestment);
+            }
+        });
+
+
+    }
+
+    public static void main(String[] args) {
+        List<String> items = new ArrayList<>();
+        items.add("A");
+        items.add("B");
+        items.add("C");
+        items.add("D");
+        items.add("E");
+
+        items.forEach(item->{
+            if("C".equals(item)){
+                System.out.println(item);
+            }
+            if("B".equals(item)){
+                System.err.println(item);
+            }
+        });
 
 
 
