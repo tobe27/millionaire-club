0612ecdbb171758a63232403a91338c5dde3345b
root <1337550831@qq.com>
2018-09-16 22:19:56 +0800

    t

--- millionaire-client-web/src/main/java/com/millionaire/millionaireclientweb/Interceptor/UserConfiguration.java
+++ millionaire-client-web/src/main/java/com/millionaire/millionaireclientweb/Interceptor/UserConfiguration.java
@@ -1,14 +1,23 @@
 package com.millionaire.millionaireclientweb.Interceptor;
 
+import com.millionaire.millionaireuserservice.service.ReceptionUsersService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.annotation.Resource;
+
 @Configuration
 public class UserConfiguration implements WebMvcConfigurer {
+    @Resource
+    private ReceptionUsersService receptionUsersService;
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new UserInterceptor()).addPathPatterns("/u/**");
+        registry.addInterceptor(new UserInterceptor(receptionUsersService)).addPathPatterns("/u/**");
     }
+
+
+
+
 
 }
